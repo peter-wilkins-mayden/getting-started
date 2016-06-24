579e8488db56135eefb4223f6de70b073a7e9ac3
Peter Wilkins <peter.wilkins@mayden.co.uk>
2016-06-24 23:02:29 +0100

    gitignore

--- resources/public/js/compiled/dev/app/mutations.cljs
+++
@@ -1,7 +0,0 @@
-(ns app.mutations
-  (:require [untangled.client.mutations :as m]))
-
-
-(defmethod m/mutate 'app/choose-tab [{:keys [state]} _ {:keys [tab]}]
-  {:action (fn []
-             (swap! state assoc :tabs [tab 1]))})
--- resources/public/js/compiled/dev/untangled/i18n/core.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.8.51 {}
-goog.provide('untangled.i18n.core');
-goog.require('cljs.core');
-untangled.i18n.core._STAR_current_locale_STAR_ = cljs.core.atom.call(null,"en-US");
-untangled.i18n.core._STAR_loaded_translations_STAR_ = cljs.core.atom.call(null,cljs.core.PersistentArrayMap.EMPTY);
-
-//# sourceMappingURL=core.js.map?rel=1466782844827
